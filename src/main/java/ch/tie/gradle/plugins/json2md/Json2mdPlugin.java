0cade992ffc51ed157aa0b1a48050b702efaa94c
fhirt <fabio_hirt@outlook.com>
2023-02-17 13:43:15 +0100

    remove automatic execution of json2md task as descriptions may be lost when building incrementally: https://github.com/spring-projects/spring-boot/issues/28075

--- src/main/java/ch/tie/gradle/plugins/json2md/Json2mdPlugin.java
+++ src/main/java/ch/tie/gradle/plugins/json2md/Json2mdPlugin.java
@@ -6,16 +6,11 @@ import org.gradle.api.Task;
 public class Json2mdPlugin implements org.gradle.api.Plugin<Project> {
 
   static final String TASK_NAME = "json2md";
-  private static final String COMPILE_JAVA = "compileJava";
 
   @Override
   public void apply(Project project) {
-    Json2mdTask json2mdTask = project.getTasks()
+    project.getTasks()
         .create(TASK_NAME, Json2mdTask.class, project, Json2mdBeanFactory.json2mdReader(project),
             Json2mdBeanFactory.json2mdWriter(project));
-    Task compileJava = project.getTasks().findByName(COMPILE_JAVA);
-    if (compileJava != null) {
-      compileJava.finalizedBy(json2mdTask);
-    }
   }
 }
