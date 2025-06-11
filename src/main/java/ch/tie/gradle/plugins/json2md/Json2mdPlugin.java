2b1a6b1f0f1412929058d7c6698e8b3b9e2c7827
Fabio Hirt <fabio_hirt@outlook.com>
2025-06-11 12:02:23 +0200

    fix deprecations

--- src/main/java/ch/tie/gradle/plugins/json2md/Json2mdPlugin.java
+++ src/main/java/ch/tie/gradle/plugins/json2md/Json2mdPlugin.java
@@ -1,7 +1,7 @@
 package ch.tie.gradle.plugins.json2md;
 
 import org.gradle.api.Project;
-import org.gradle.api.Task;
+import org.gradle.api.tasks.TaskProvider;
 
 public class Json2mdPlugin implements org.gradle.api.Plugin<Project> {
 
@@ -9,8 +9,14 @@ public class Json2mdPlugin implements org.gradle.api.Plugin<Project> {
 
   @Override
   public void apply(Project project) {
-    project.getTasks()
-        .create(TASK_NAME, Json2mdTask.class, project, Json2mdBeanFactory.json2mdReader(project),
+
+    TaskProvider<Json2mdTask> json2mdTaskTaskProvider = project.getTasks()
+        .register(TASK_NAME, Json2mdTask.class, project, Json2mdBeanFactory.json2mdReader(project),
             Json2mdBeanFactory.json2mdWriter(project));
+    json2mdTaskTaskProvider.configure(task -> {
+      task.setGroup("documentation");
+      task.setDescription("Generates markdown documentation from Spring configuration metadata JSON files.");
+      task.dependsOn("compileJava");
+    });
   }
 }
